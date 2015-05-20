11ac7e525f86ee2e50d575d5a81c6c1fb4efda08
Ilya Ashchepkov <koctep@gmail.com>
2015-05-20 18:17:45 +0000

    clean debian dir

--- debian/postgresql-9.4-jsonb-extend/usr/share/postgresql/9.4/extension/jsonb_extend--unpackaged--1.0.sql
+++
@@ -1,7 +0,0 @@
-/* contrib/test_parser/test_parser--unpackaged--1.0.sql */
-
--- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION jsonb_extend" to load this file. \quit
-
-ALTER EXTENSION jsonb_extend ADD function jsonb_extend(jsonb,jsonb);
-ALTER EXTENSION jsonb_extend ADD function jsonb_deep_extend(jsonb,jsonb,bool);
